51383b7d74aa5d01ca13c9cdf5f76dc376383135
Eelco <eelcoschreurs@gmail.com>
2023-09-08 16:34:08 +0200

    First commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
+
+
+# Wait until Postgres is ready before running the next step.
+
+while ! pg_isready -q -h $DB_HOST -p $DB_PORT -U $POSTGRES_USER
+
+do
+
+    echo "$(date) - waiting for database to start."
+    sleep 2
+
+done
+
+
+# # If the database exists, migrate. Otherwise setup (create and migrate)
+
+echo "Running database migrations..."
+
+bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup db:migrate
+
+echo "Finished running database migrations."
+
+echo "Running packages update..."
+bundle exec rails packages:update
+echo "Finished packages update."
+
+# Generate Admin account
+bundle exec rake admin_generator
+
+bundle exec rails s -b 0.0.0.0 -p 3000
+
