1cfd135fb88f0ed8eb66031377533f67c70dc05a
Eelco <eelcoschreurs@gmail.com>
2023-09-12 13:20:04 +0200

    server.pid path updated
    removing the correct server.pid file on the correct path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
+rm -f /usr/src/app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
